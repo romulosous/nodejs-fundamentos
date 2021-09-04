172c77d9b44cb53e2496a74ab04444fa8b26ee0f
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-09-04 11:41:43 -0300

    middlewares

--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ app.use(express.json());
 
 const customers = [];
 
-/**
- * cpf - string
- * name - string
- * id - uuid
- * statement []
- */
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+  request.customer = customer;
+
+  return next();
+}
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
@@ -35,24 +42,10 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-// app.get("/statement/:cpf", (request, response) => {
-//   const { cpf } = request.params;
-//   const customer = customers.find((customer) => customer.cpf === cpf);
-//   console.log(customer);
-//   if (!customer) {
-//     return response.status(400).json({ error: "Customer not found" });
-//   }
+// app.use(verifyIfExistsAccountCPF)
 
-//   return response.json(customer.statement);
-// });
-
-app.get("/statement/", (request, response) => {
-  const { cpf } = request.headers;
-  const customer = customers.find((customer) => customer.cpf === cpf);
-  console.log(customer);
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found" });
-  }
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
 
   return response.json(customer.statement);
 });
